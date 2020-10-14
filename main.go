31a5613931c4bf4b0c9770c1c64c808f1be5bd67
八木浩樹 <yagihiroki@yagipc.local>
2020-10-14 19:14:54 +0900

    uart

--- main.go
+++ main.go
@@ -18,8 +18,47 @@ const (
 )
 
 var CLI struct {
-	BaudRate int    `required help:"Baud rate" short:"b"`
 	Path     string `arg name:"path" help: "USB device path." type:"path"`
+	BaudRate int    `required help:"Baud rate" short:"b"`
+	Repl     bool   `help:"launch repl mode" default:"false" short:"r"`
+}
+
+func repl(s *serial.Port) error {
+	rl, err := readline.NewEx(&readline.Config{
+		Prompt:          fmt.Sprintf("%s>%s ", COLOR_GREEN, COLOR_OFF),
+		InterruptPrompt: fmt.Sprintf("\n%sinterrupt%s", COLOR_RED, COLOR_OFF),
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rl.Close()
+
+	buf := make([]byte, 128)
+
+	for {
+		line, err := rl.Readline()
+		if err != nil {
+			return err
+		}
+
+		if len(line) == 0 {
+			continue
+		}
+
+		n, err := s.Write([]byte(line)[:1])
+		if err != nil {
+			return err
+		}
+		fmt.Printf("  write %d byte\n", n)
+
+		n, err = s.Read(buf)
+		if err != nil {
+			return err
+		}
+		fmt.Printf("%q\n", buf[:n])
+	}
+
+	return nil
 }
 
 func main() {
@@ -37,33 +76,24 @@ func main() {
 	}
 	defer s.Close()
 
-	rl, err := readline.NewEx(&readline.Config{
-		Prompt:          fmt.Sprintf("%s>%s ", COLOR_GREEN, COLOR_OFF),
-		InterruptPrompt: fmt.Sprintf("\n%sinterrupt%s", COLOR_RED, COLOR_OFF),
-	})
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer rl.Close()
+	if CLI.Repl {
+		err := repl(s)
+		if err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		buf := make([]byte, 128)
 
-	buf := make([]byte, 128)
-
-	for {
-		line, err := rl.Readline()
+		n, err := s.Write([]byte("Hello, World!"))
+		fmt.Printf("(write %d byte)\n", n)
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		if len(line) == 0 {
-			continue
-		}
-
-		s.Write([]byte(line)[:1])
-
-		n, err := s.Read(buf)
+		n, err = s.Read(buf)
 		if err != nil {
 			log.Fatal(err)
 		}
-		fmt.Printf("%q\n", buf[:n])
+		fmt.Printf("%s\n", buf[:n])
 	}
 }
